fafa45c6e46890082a0f426cd1de10b2e6b7ae7a
Aaron Burtnyk <aaron.burtnyk@candide.com>
2024-09-15 19:35:41 +0100

    Switch jest -> vitest

--- src/__tests__/utils.test.ts
+++ src/__tests__/utils.test.ts
@@ -1,4 +1,4 @@
-import {expect, describe, it} from "@jest/globals"
+import {expect, describe, it} from "vitest"
 
 import {Collection, Route, WebSocketHandler, StaticPathOptions} from "../types"
 import {
